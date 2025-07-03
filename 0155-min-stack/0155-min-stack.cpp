8f06c7c3ebc9eb23f5372b10272c70de52c6b1da
loki-l1 <146949459+loki-l1@users.noreply.github.com>
2025-07-03 17:34:05 +0530

    Time: 0 ms (100%), Space: 23.7 MB (22.79%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,6 +1,7 @@
 class MinStack {
 public:
-stack<pair<int,int>> s;
+stack<long long int> s;
+long long int minVal;
     MinStack() {
         
     }
@@ -8,26 +9,45 @@ stack<pair<int,int>> s;
     void push(int val) {
         if(s.empty())
         {
-            s.push({val,val});
+            s.push(val);
+            minVal=val;
         }
         else
         {
-            int minVal=min(val,s.top().second);
-            s.push({val,minVal});
+            if(val<minVal)
+            {
+                s.push((long long)2*val-minVal);
+                minVal=val;
+            }
+            else
+            {
+                s.push(val);
+            }
         }
     }
     
     void pop() {
-
+        if(s.top()<minVal)
+        {
+            minVal=2*minVal-s.top();
+            
+        }
         s.pop();
     }
     
     int top() {
-        return s.top().first;  
+        if(s.top()<minVal)
+        {
+            return minVal;
+        }  
+        else
+        {
+            return s.top();
+        }
     }
     
     int getMin() {
-        return s.top().second;
+        return minVal;;
     }
 };
 
